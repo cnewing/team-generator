4ebd3136dd28ab4ec6c6d5195dc9208f326b945d
Christa <christanewing47@gmail.com>
2022-08-24 20:35:39 -0400

    initial employee test

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -17,4 +17,17 @@ test("Get employee name", () => {
 test("Get employee ID", () => {
   const empId = 659;
   const employeeNew = new Employee("Alicia", empId);
+  expect(employeeNew.empId).toBe(empId);
 });
+
+// G E T  E M P L O Y E E  E M A I L
+test("Get employee email", () => {
+  const email = "alicia.degrassi@gmail.com";
+  const employeeNew = new Employee("Alicia", 659, email);
+  expect(employeeNew.email).toBe(email);
+});
+
+// G E T  E M P L O Y E E  R O L E
+test("Get employee role", () => {});
+const returnEmp = "Employee";
+expect(employeeNew.getRole()).toBe(returnEmp);
